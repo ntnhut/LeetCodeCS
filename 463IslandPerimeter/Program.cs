71cc26ab00af6b2df8292bf3e76bfc9ef79c4475
Nhut Nguyen <ntnhut17@gmail.com>
2021-10-05 13:59:12 +0200

    count number of subsequences to compute the perimeter

--- 463IslandPerimeter/Program.cs
+++ 463IslandPerimeter/Program.cs
@@ -8,20 +8,34 @@ namespace _463IslandPerimeter
 {
     public class Solution
     {
-        public int IslandPerimeter(int[,] grid)
+        public int IslandPerimeter(int[][] grid)
         {
             int result = 0;
-            for (int i=0; i < grid.GetLength(0); i++)
+            for (int i = 0; i < grid.Length; i++)
             {
-                for (int j=0; j< grid.GetLength(1)-1; j++)
+                for (int j = 0; j < grid[i].Length; j++)
                 {
-                    if (grid[i,j]!=grid[i,j+1])
+                    if (grid[i][j] == 1)
                     {
-                        result++;
+                        if (j == 0)
+                        {
+                            result += 2;
+                        } else if (grid[i][j-1]==0)
+                        {
+                            result += 2;
+                        }
+                        if (i==0)
+                        {
+                            result += 2;
+                        }
+                        else if (grid[i-1][j]==0)
+                        {
+                            result += 2;
+                        }
                     }
                 }
             }
-            return 0;
+            return result;
         }
     }
     class Program
@@ -29,8 +43,19 @@ namespace _463IslandPerimeter
         static void Main(string[] args)
         {
             var so = new Solution();
-            var grid = new int[,] { { 0, 1, 0, 0 }, { 1, 1, 1, 0 }, { 0, 1, 0, 0 }, { 1, 1, 0, 0 } };
-            Console.WriteLine(so.IslandPerimeter(grid));
+            var grid1 = new int[][] 
+            { 
+                new int[] { 0, 1, 0, 0 }, 
+                new int[] { 1, 1, 1, 0 }, 
+                new int[] { 0, 1, 0, 0 }, 
+                new int[] { 1, 1, 0, 0 } 
+            };
+            Console.WriteLine(so.IslandPerimeter(grid1)); // 16
+            var grid2 = new int[][] 
+            { 
+                new int[] { 1 } 
+            };
+            Console.WriteLine(so.IslandPerimeter(grid2)); // 4
         }
     }
 }
