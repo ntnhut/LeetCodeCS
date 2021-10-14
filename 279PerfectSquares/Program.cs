f2d0526f0a5893a9a46d2d7af7fbd1c33c48d694
Nhut Nguyen <ntnhut17@gmail.com>
2021-10-14 14:42:38 +0200

    use Lagrange's Four Square Theorem

--- 279PerfectSquares/Program.cs
+++ 279PerfectSquares/Program.cs
@@ -5,24 +5,37 @@ namespace _279PerfectSquares
 {
     public class Solution
     {
-        Dictionary<int, int> _ns = new Dictionary<int, int>();
+        public bool isSquare(int n)
+        {
+            var sq = (int)Math.Sqrt(n);
+
+            return (sq * sq == n);
+        }
+        // Due to Lagrange's Four Square Theorem, the result is at most 4.
         public int NumSquares(int n)
         {
-            if (_ns.ContainsKey(n)) return _ns[n];
             var sq = (int)Math.Sqrt(n);
 
             if (sq * sq == n)
             {
-                _ns.Add(n,1);
                 return 1;
             }
-            int result = n;
+            // Due to Legendre's three-square theorem, 
+            // the result cannot be 1, 2, or 3 if n is of the form n = 4^a(8*b + 7)
+
+            while (n%4==0)
+            {
+                n /= 4;
+            }
+            if (n %8 == 7)
+            {
+                return 4;
+            }
             for (int i=1; i<= sq; i++)
             {
-                result = Math.Min(result, NumSquares(n - i * i));
+                if (isSquare(n - i * i)) return 2;
             }
-            _ns.Add(n, result + 1);
-            return _ns[n];
+            return 3;
         }
     }
     class Program
